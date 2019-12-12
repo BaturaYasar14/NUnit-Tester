f4da3f00eafa415e04240d6a8828851e70755d81
BaturaYasar14 <baturaYasar14@users.noreply.github.com>
2019-12-12 18:33:38 +0100

    Using parse instead of if else conditions

---
+++ NUnit Tester/RPNCalculater.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_Tester
+{
+    class RPNCalculater
+    {
+        
+        public double Evaluate(string input)
+        {
+            return int.Parse(input);
+        }
+       
+    }
+}
---
+++ NUnit Tester/RPNCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnit_Tester
+{
+    class RPNCalculatorTests
+    {
+        [Test]
+        public void TestSingleDigit()
+        {
+            RPNCalculater calc = new RPNCalculater();
+            Assert.That(calc.Evaluate("3"), Is.EqualTo(3));
+        }
+        [Test]
+        public void TestSingleDigit2()
+        {
+            RPNCalculater calc = new RPNCalculater();
+            Assert.That(calc.Evaluate("4"), Is.EqualTo(4));
+        }
+        [Test]
+        public void TestSingleDigit3()
+        {
+            RPNCalculater calc = new RPNCalculater();
+            Assert.That(calc.Evaluate("5"), Is.EqualTo(5));
+        }
+    }
+}
